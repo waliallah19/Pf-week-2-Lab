ce8894ead21b54473465d4409e4230dc97d3a1fa
DESN <phs.wali319@gmail.com>
2022-12-16 12:02:13 +0500

    PF week 2 Lab

---
+++ ProjectHeader.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout <<"***************************************************************************************************************" <<endl;
+cout <<"*                                                                                                             *" <<endl;
+cout <<"*                                              Punjab High School                                             *" <<endl;
+cout <<"*                                         Admission Mangagement System                                        *" <<endl;
+cout <<"*                                                                                                             *" <<endl;
+cout <<"***************************************************************************************************************" <<endl;
+return 0;
+}
